440c1828bc9ad2d831570a4cf6115b9f759a2736
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-03-30 18:47:44 -0300

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='templates')
-
-@app.route('/')
-def index():
+app = Flask(__name__, 
+            template_folder='templates', 
+            static_folder='static')
     return render_template('index.html')
 
 if __name__ == '__main__':
