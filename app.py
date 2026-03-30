657cc117a707234d5e9ede33b9288de16cd7dc1e
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-03-30 13:18:31 -0300

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Este código ensina o Python a achar as pastas dentro de 'modelos'
+app = Flask(__name__, 
+            template_folder='modelos', 
+            static_folder='modelos/static/imagens')
 
-@app.route("/")
-def home():
- return render_template("index.html")
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
