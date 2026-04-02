23ad46339f7a7bb2f5264f2b92c3b3c04d419e6e
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-04-02 13:13:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # O Flask procura na pasta /templates
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Isso garante que o servidor escolha a porta certa automaticamente
+    # O Render exige que usemos a porta que ele fornece
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
