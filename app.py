d7aeb5f57cb788f4ff16845466ccac27201778fb
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-04-02 13:19:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+# Forçamos o Flask a reconhecer as pastas de templates e arquivos estáticos
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Usar a porta 5001 caso a 5000 esteja ocupada, isso evita conflitos
-    app.run(debug=True, port=5001)
+    # O Render exige que usemos a porta fornecida pela variável de ambiente PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
