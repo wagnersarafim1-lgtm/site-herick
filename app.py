e7c752e8bc9459758f90238cc032537489e75b4b
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-04-02 12:52:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # O Flask vai carregar o index.html da pasta /templates
-    # Certifique-se de que o arquivo se chama index.html
+    # Isso vai carregar o seu HTML que corrigimos abaixo
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # debug=True permite que você veja os erros no navegador e reinicie o servidor automaticamente ao salvar
     app.run(debug=True)
