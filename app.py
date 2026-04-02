07c0b509ac372d3fd625074a3cbcff3988198050
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-04-02 13:15:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Força o Flask a entender a pasta static e templates no servidor Linux do Render
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # O Render exige que usemos a porta que ele fornece
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
