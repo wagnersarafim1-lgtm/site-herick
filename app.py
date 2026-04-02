082e8126a6b3573453aef12c810ec46c5adbdf03
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-04-02 13:09:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # O servidor web vai definir a porta automaticamente, por isso usamos o environ.get
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
