e3bf7bea292864b9a11645874d54ee426e4562dd
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-04-01 21:29:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, 
-            template_folder='templates', 
-            static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
