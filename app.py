0cc9e015d681ee63fb3ba9b85f332695a0e4baa2
wagnersarafim1-lgtm <wagnersarafim1@gmail.com>
2026-04-02 12:32:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # O Flask procura automaticamente na pasta 'templates'
     return render_template('index.html')
 
 if __name__ == '__main__':
+    # rodando em modo debug para você ver o erro real no terminal
     app.run(debug=True)
